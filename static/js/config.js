5978f495b41a79b96f54f40237fb7425c2857eea
araethomp <angela.r.thomp@gmail.com>
2021-05-25 13:45:48 -0700

    Create config file, start of logic.js code

---
+++ static/js/config.js
@@ -0,0 +1,3 @@
+// API key
+
+const API_key = "pk.eyJ1IjoiYXJhZXRob21wIiwiYSI6ImNrbmZjOWxxZTAxbWEydW50NWRvejRxNWoifQ.D4VUKLhtilQz2-Zd5is-tw"
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,19 +1,21 @@
-/* NOTE FOR STEP 2
-/  You can use the javascript Promise.all function to make two d3.json calls, 
-/  and your then function will not run until all data has been retreived.
-/
-/ ----------------------------------------
-/  Promise.all(
-/    [
-/        d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"),
-/        d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json")
-/    ]
-/  ).then( ([data,platedata]) => {
-/
-/        console.log("earthquakes", data)
-/        console.log("tectonic plates", platedata)
-/
-/    }).catch(e => console.warn(e));
-/
-/ ----------------------------------------*/
+// Store the API endpoint
+const queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson"
 
+// GET request for queryUrl
+d3.json(queryUrl).then(data => {
+    console.log(data);
+    console.log(d3.extent(data.features.map(d => d.properties.mag)))
+    makeFeatures(data.features);
+});
+
+// Make function to run for each feature then give each feature a descriptive popup
+function makeFeatures(earthquakeInfo) {
+    function eachFeature(feature, layer) {
+        layer.bindPopup("<h3>" + feature.properties.title +
+        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+    }
+    // GeoJSON layer that holds the features array on the earthquakeInfo object then run the eachFeature function for each piece of data
+    const earthquakes = L.geoJSON(earthquakeInfo, {
+        eachFeature: eachFeature,
+    });
+}
