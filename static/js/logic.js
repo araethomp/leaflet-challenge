0416c6b28475b3a95b3b2b71b67cd856ac8bb7d4
araethomp <angela.r.thomp@gmail.com>
2021-05-28 11:34:27 -0700

    Finishing up logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -66,6 +66,11 @@ function createMap(earthquakes) {
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
     }).addTo(earthquakeMap);
+
+    // Create legend
+    let legend = L.control({
+        position: "bottomright"
+    });
 }
 // Organize and assign colors according to magnitude 
 function magsColor(d) {
@@ -90,71 +95,3 @@ function magsColor(d) {
 function magSize(value) {
     return value*20000
 };
-
-// function circleSize(features, latlng){
-//     let circles = {
-//         radius: features.properties.mag * 10, 
-//         fillColor: magsColor(feature.properties.mag),
-//         color: magsColor(feature.properties.mag)
-//     }
-// }
-//     // GeoJSON layer that holds the features array on the earthquakeData object then run the onEachFeature function for each piece of data
-//     const earthquakes = L.geoJSON(earthquakeData, {
-//         onEachFeature: onEachFeature
-//     });
-//     const mags = L.geoJSON(earthquakeData, {
-//         onEachFeature: onEachFeature,
-//         layerPoint: (feature, latlng) => {
-//             return new L.Circle(latlng, {
-//                 radius: feature.properties.mag*20000, 
-//                 fillOpacity: 2,
-//                 stroke: false
-//             });
-//         }
-//     });
-//     // Add earthquakes layer
-//     createMap(earthquakes, mags);
-// }
-// // Establish createMap function
-// function createMap(earthquakes, mags) {
-//     // Add layer options
-//     const streetmapLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-//         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-//         tileSize: 512,
-//         maxZoom: 18,
-//         zoomOffset: -1,
-//         id: "mapbox/streets-v11",
-//         accessToken: API_key
-//     });
-//     const darkmapLayer = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-//         attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
-//        maxZoom: 18,
-//        id: "dark-v10",
-//        accessToken: API_key
-//    }); 
-//     // Create object to hold base layers
-//     const baseMaps = {
-//         "Street Map Layer": streetmapLayer,
-//         "Dark Map Layer": darkmapLayer
-//     };
-//     // Create object to hold overlay layer
-//     const overlayMaps = {
-//         Earthquakes: earthquakes,
-//         Magnitudes: mags
-//     };
-//     // Create map with streetmap layer and earthquake layers for the display on page load
-//     const loadMap = L.map("map", {
-//         center: [40, -98], 
-//         zoom: 3,
-//         layers: [streetmapLayer, earthquakes]
-//     });
-//     //  Create and add layer control, pass in baseMaps and overlayMaps
-//     L.control.layers(baseMaps, overlayMaps, {
-//         collapsed: false
-//     }).addTo(loadMap);
-//     // Add legend
-//     let legend = L.control(
-//        { position: "bottomleft" }
-//     )
-//     legend.addTo(loadMap);
-// }
